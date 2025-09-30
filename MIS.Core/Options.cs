d0b03bb07cec35efbbe88a5545b2b9f15862c309
andybykov <123543480+andybykov@users.noreply.github.com>
2025-09-30 22:11:43 +0300

    Update Options.cs

--- MIS.Core/Options.cs
+++ MIS.Core/Options.cs
@@ -6,11 +6,7 @@
         public static string ConnectionString
         {  get 
             {
-                //return  "Server = localhost;" +
-                //        " Port = 5432;" +
-                //        " User Id = postgres;" +
-                //        " Password = 4770;" +
-                //        " Database = MIS;";
+                
 
                 // Получаем данные Connection string из "переменной среды"
                 string? cs = Environment.GetEnvironmentVariable("ConnectionStringDB");
